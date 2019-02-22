02b0f9fe9e03e21aa5644692324b77a5f7f1a19f
aej11a <aej11@hotmail.com>
2019-02-22 17:05:24 -0500

    Initial Commit

---
+++ WashBot/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  WashBot
+//
+//  Created by Andrew Jones on 2/22/19.
+//  Copyright © 2019 WashBot. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
